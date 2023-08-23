8f5b4bc03aa83524a92761cbaa9b5726b4c143f4
alexandrapanastier <panastier.alexandra@gmail.com>
2023-08-23 15:28:06 +0200

    first algo

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+// EXERCICE 1
+// Crée une fonction qui prend deux paramètres (a et b) et retourne la somme multipliée des deux paramètres. En php, une fonction se déclare avec le mot-clé function. Et les paramètres ont un $ devant. Comme les variables. Pour déclarer une variable ont met toujours $ devant. Pour retourner une valeur, on utilise le mot-clé return. Exemple : return $a + $b;
+// En Php on a pas de console.log() pour afficher du texte. On utilise echo à la place. Exemple : echo "Hello world !";
+
+// Maintenant pour afficher le resultat dans ton terminal fait php index.php (index.php est le nom de ton fichier, ça ressemble à ce que tu fais en JS avec node index.js)
+
+
+
+
+
+
+
+
+
+
+
+// ========== SOLUTION ==========
+// function calculSomme($a, $b) {
+//     $somme = $a + $b;
+//     return $somme;
+// }
+
+// echo calculSommeMultipliee(2, 3); // Affiche 5
+
+// EXERCICE 2
+
+// Crée une fonction qui prend en paramètre un tableau et qui retourne le premier élément du tableau.
+// Exemple : [1, 2, 3] => 1
+// La logique entre le JS et le PHP est la même. Tu peux également faire [0] pour récupérer le premier élément d'un tableau.
+
+// ========== SOLUTION ==========
+// function firstElementTab($tab) {
+//     return $tab[0];
+// }
+// echo firstElementTab([1, 2, 3]); // Affiche 1
+
+?>
